c5a09aee9badbf855cba4800f76edc682cf1c54f
liuchang <liuchang@orbbec.com>
2017-12-06 14:52:52 +0800

    解相位

---
+++ PhaseUnwrapping/main.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main()
+{
+	int n = 29 - 1;
+	int imgWidth = 1280;
+	int imgHeight = 800;
+	Mat I(imgHeight, imgWidth,CV_32F,0);
+	int direction = 0;
+	int movement = 20;
+	int pixelWidth = 20;
+
+	return 0;
+}
